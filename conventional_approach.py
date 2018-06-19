28e5a1f6650de403bc87117e3033533b43e95d8c
Florian Wagner <mail@fwagner.info>
2018-06-19 14:12:52 +0200

    Current working version with porosity constraint and LSQR inversion

--- conventional_approach.py
+++ conventional_approach.py
@@ -1,3 +1,5 @@
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -28,13 +30,22 @@ pg.show(mesh, markers=True)
 # %%
 mesh.save("mesh.bms")
 
-rholayers = np.array([0, 8000, 200000, 2000, 5000])  # 0 is a dummy value
-vellayers = np.array([0, 1750, 3500, 2000, 4000])  # 0 is a dummy value
+fpm = FourPhaseModel()
+
+rholayers = np.array([0, 8000, 200000, 2000, 6000])  # 0 is a dummy value
+vellayers = np.array([0, 1750, 3500, 2000, 4200])  # 0 is a dummy value
+
+# Model creation based on pore fractions
+# fwlayers = np.array([0, 0.05, 0, 0.2, 0.05])
+# filayers = np.array([0, 0.2, 0.37, 0, 0.37])
+# falayers = np.ones(len(fwlayers)) - fwlayers - filayers
+#
+# rholayers = fpm.rho(fwlayers, filayers, falayers)
+# rholayers = fpm.slowness(fwlayers, filayers, falayers)
 
 rhotrue = rholayers[mesh.cellMarkers()]
 veltrue = vellayers[mesh.cellMarkers()]
 
-fpm = FourPhaseModel()
 fa, fi, fw, mask = fpm.all(rhotrue, veltrue)
 np.savez("true_model.npz", rho=rhotrue, vel=veltrue, fa=fa, fi=fi, fw=fw)
 
@@ -48,8 +59,10 @@ ertScheme = pb.createData(sensors, "dd")
 ert = ERTManager()
 
 # Create suitable mesh for ert forward calculation
-meshERT = mt.createParaMesh(ertScheme, quality=33, paraMaxCellSize=1.0, paraDX=0.1,
-                            boundaryMaxCellSize=50, smooth=[1, 2], paraBoundary=30)
+meshERT = mt.createParaMesh(ertScheme, quality=33.5, paraMaxCellSize=1.0, paraDX=0.2,
+                            boundaryMaxCellSize=50, smooth=[1, 10], paraBoundary=30)
+
+pg.show(meshERT)
 
 res = pg.RVector()
 pg.interpolate(mesh, rhotrue, meshERT.cellCenters(), res)
@@ -57,29 +70,45 @@ res = mt.fillEmptyToCellArray(meshERT, res, slope=True)
 # pg.show(meshERT, res)
 ert.setMesh(meshERT)
 ert.fop.createRefinedForwardMesh()
-ertData = ert.simulate(meshERT, res, ertScheme, noiseLevel=0.01, noiseAbs=0.0)
+ertData = ert.simulate(meshERT, res, ertScheme, noiseLevel=0.05, noiseAbs=0.0)
 ertData.save("erttrue.dat")
 ert.setData(ertData)
 ert.setMesh(meshERT)
 ert.inv.setData(ertData("rhoa"))
-ert.fop.regionManager().setZWeight(0.5)
-resinv = ert.invert(ertData, mesh=meshERT, lam=5, zWeight=0.5)
+# ert.fop.regionManager().setZWeight(0.5)
+
+meshERT = mt.createParaMesh(ertScheme, quality=33.5, paraMaxCellSize=1.0, paraDX=0.2,
+                            boundary=50, smooth=[1, 10], paraBoundary=3)
+pg.show(meshERT, markers=True)
+# CM = pg.utils.geostatistics.covarianceMatrix(ert.paraDomain, I=[40, 3])
+# ert.fop.setConstraints(pg.matrix.Cm05Matrix(CM))
+
+resinv = ert.invert(ertData, mesh=meshERT, lam=5, zWeight=1)
+
 print("ERT chi:", ert.inv.chi2())
 print("ERT rms:", ert.inv.relrms())
 # resinv = ert.inv.runChi1()
 rhoest = pg.interpolate(ert.paraDomain, resinv, mesh.cellCenters())
+ert_cov = pg.interpolate(ert.paraDomain, ert.coverageDC(), mesh.cellCenters())
+ert_cov.save("ert_coverage.dat")
+
+
 
 print("-Simulate refraction seismics" + "-" * 50)
+meshRST = pg.Mesh()
+meshRST.createMeshByMarker(meshERT, 2)
+
 vel = pg.RVector()
-pg.interpolate(mesh, veltrue, meshERT.cellCenters(), vel)
-vel = mt.fillEmptyToCellArray(meshERT, vel, slope=False)
+pg.interpolate(mesh, veltrue, meshRST.cellCenters(), vel)
+vel = mt.fillEmptyToCellArray(meshRST, vel, slope=False)
 
 ttScheme = createRAData(sensors)
 rst = Refraction(verbose=True)
 # rst.useFMM(True)
-# rst.setMesh(meshERT)
+# rst.setMesh(meshRST)
 # rst.fop.createRefinedForwardMesh()
-ttData = rst.simulate(meshERT, 1. / vel, ttScheme, noisify=True, noiseLevel=0.001, noiseAbs=0.0001)
+
+ttData = rst.simulate(meshRST, 1. / vel, ttScheme, noisify=True, noiseLevel=0.0001, noiseAbs=0.001)
 
 rst.setData(ttData)
 rst.dataContainer.save("tttrue.dat")
@@ -88,15 +117,21 @@ ttData = rst.dataContainer
 rst.createMesh(depth=15., paraMaxCellSize=1., quality=33.5, boundary=0, smooth=[1,10],
                paraDX=0.1, paraBoundary=3)
 rst.mesh.save("paraDomain.bms")
-rst.fop.regionManager().setZWeight(0.5)
+# rst.fop.regionManager().setZWeight(0.5)
 rst.inv.setData(ttData("s"))
 rst.inv.setMaxIter(50)
 # ttData.set("err", np.ones(len(ttData("s"))) * 0.001)
 # rst.inv.setRelativeError(0.001)
-vest = rst.invert(ttData, zWeight=0.8, lam=15, useGradient=True, vtop=1000, vbottom=4000)
+
+# CM = pg.utils.geostatistics.covarianceMatrix(rst.mesh, I=[40, 3])
+# rst.fop.setConstraints(pg.matrix.Cm05Matrix(CM))
+vest = rst.invert(ttData, zWeight=1, lam=15, useGradient=True, vtop=1000, vbottom=4000)
 # vest = rst.inv.runChi1()
 print("RST chi:", rst.inv.chi2())
 print("RST rms:", rst.inv.relrms())
 velest = pg.interpolate(rst.mesh, vest, mesh.cellCenters())
 fae, fie, fwe, maske = fpm.all(rhoest, velest)
 np.savez("conventional.npz", vel=velest, rho=rhoest, fa=fae, fi=fie, fw=fwe)
+
+rst_cov = pg.interpolate(rst.paraDomain(), rst.rayCoverage(), mesh.cellCenters())
+rst_cov.save("rst_coverage.dat")
--- plot.py
+++ plot.py
@@ -8,7 +8,7 @@ from matplotlib import ticker
 
 import pybert as pb
 import pygimli as pg
-from pygimli.mplviewer import drawModel as draw
+from pygimli.mplviewer import drawModel, addCoverageAlpha
 
 seaborn.set(font="Fira Sans", style="ticks")
 plt.rcParams["image.cmap"] = "viridis"
@@ -80,33 +80,98 @@ grid = ImageGrid(fig, 111, nrows_ncols=(6, 3), axes_pad=0.15, share_all=True,
                  add_all=True, cbar_location="right", cbar_mode="edge",
                  cbar_size="5%", cbar_pad=0.15, aspect=True)
 
-im = draw(grid.axes_row[0][0], mesh, veltrue, **lim(veltrue), logScale=False)
-im = draw(grid.axes_row[0][1], mesh, velest, **lim(veltrue), logScale=False)
-im = draw(grid.axes_row[0][2], meshj, veljoint, **lim(veltrue), logScale=False)
+ert_cov = np.loadtxt("ert_coverage.dat")
+rst_cov = np.loadtxt("rst_coverage.dat")
+
+ert_covj = pg.interpolate(mesh, ert_cov, meshj.cellCenters()).array()
+rst_covj = pg.interpolate(mesh, rst_cov, meshj.cellCenters()).array()
+
+# Extract convex hull
+
+from scipy.spatial import ConvexHull
+from matplotlib.path import Path
+
+points_all = np.column_stack((
+    pg.x(mesh.cellCenters()),
+    pg.y(mesh.cellCenters()),
+))
+
+points = points_all[np.nonzero(rst_cov)[0]]
+hull = ConvexHull(points)
+hull_path = Path(points[hull.vertices])
+
+covs = []
+for cell in mesh.cells():
+    if not hull_path.contains_point(points_all[cell.id()]):
+        covs.append(ert_cov[cell.id()])
+
+def joint_cov(ert_cov, rst_cov, mesh):
+    """ Joint ERT and RST coverage for visualization. """
+
+    points_all = np.column_stack((
+        pg.x(mesh.cellCenters()),
+        pg.y(mesh.cellCenters()),
+    ))
+    cov = np.array(ert_cov)
+    for cell in mesh.cells():
+        if hull_path.contains_point(points_all[cell.id()]):
+            cov[cell.id()] += np.max(covs)
+
+    return cov
+
+covj = joint_cov(ert_covj, rst_covj, meshj)
+cov = joint_cov(ert_cov, rst_cov, mesh)
+
+# for cell in mesh.cells():
+#     if 10**ert_cov[cell.id()] > 10**ert_cov.max() * 0.03:
+#         rst_cov[cell.id()] = 1
+#
+# cov[np.nonzero(rst_cov)[0]] = ert_cov.max()
+# cov[np.nonzero(rst_cov)[0]] = ert_cov.max()
+# cov[np.nonzero(rst_cov)[0]] += ert_cov.max()
+
+def draw(ax, mesh, model, **kwargs):
+    gci = drawModel(ax, mesh, model, **kwargs)
+    if "coverage" in kwargs:
+        addCoverageAlpha(gci, kwargs["coverage"])
+        pass
+
+    # for simplex in hull.simplices:
+    #     x = points[simplex, 0]
+    #     y = points[simplex, 1]
+    #     if (y < 0.1).all():
+    #         ax.plot(x, y, 'w-', lw=1.5, alpha=0.5)
+
+    return gci
+
+
+im = draw(grid.axes_row[0][0], mesh, veltrue, cmap="viridis", **lim(veltrue), logScale=False)
+draw(grid.axes_row[0][1], mesh, velest, cmap="viridis",  **lim(veltrue), logScale=False, coverage=cov)
+draw(grid.axes_row[0][2], meshj, veljoint, cmap="viridis", **lim(veltrue), logScale=False, coverage=covj)
 cb = fig.colorbar(im, cax=grid.cbar_axes[0])
 update_ticks(cb, label=labels[0])
 
 im = draw(grid.axes_row[1][0], mesh, rhotrue, cmap="Spectral_r", **lim(rhotrue), logScale=True)
-im = draw(grid.axes_row[1][1], mesh, rhoest, cmap="Spectral_r", **lim(rhotrue), logScale=True)
-im = draw(grid.axes_row[1][2], meshj, rhojoint, cmap="Spectral_r", **lim(rhotrue), logScale=True)
+draw(grid.axes_row[1][1], mesh, rhoest, cmap="Spectral_r", **lim(rhotrue), logScale=True, coverage=cov)
+draw(grid.axes_row[1][2], meshj, rhojoint, cmap="Spectral_r", **lim(rhotrue), logScale=True, coverage=covj)
 cb = fig.colorbar(im, cax=grid.cbar_axes[1])
 update_ticks(cb, log=True, label=labels[1])
 
 im = draw(grid.axes_row[2][0], mesh, fa, logScale=False, cmap="Greens", **lim(fa))
-im = draw(grid.axes_row[2][1], mesh, fae, logScale=False, cmap="Greens", **lim(fa))
-im = draw(grid.axes_row[2][2], meshj, faj, logScale=False, cmap="Greens", **lim(fa))
+draw(grid.axes_row[2][1], mesh, fae, logScale=False, cmap="Greens", **lim(fa), coverage=cov)
+draw(grid.axes_row[2][2], meshj, faj, logScale=False, cmap="Greens", **lim(fa), coverage=covj)
 cb = fig.colorbar(im, cax=grid.cbar_axes[2])
 update_ticks(cb, label=labels[2])
 
 im = draw(grid.axes_row[3][0], mesh, fi, logScale=False, cmap="Purples", **lim(fi))
-im = draw(grid.axes_row[3][1], mesh, fie, logScale=False, cmap="Purples", **lim(fi))
-im = draw(grid.axes_row[3][2], meshj, fij, logScale=False, cmap="Purples", **lim(fi))
+draw(grid.axes_row[3][1], mesh, fie, logScale=False, cmap="Purples", **lim(fi), coverage=cov)
+draw(grid.axes_row[3][2], meshj, fij, logScale=False, cmap="Purples", **lim(fi), coverage=covj)
 cb = fig.colorbar(im, cax=grid.cbar_axes[3])
 update_ticks(cb, label=labels[3])
 
 im = draw(grid.axes_row[4][0], mesh, fw, logScale=False, cmap="Blues", **lim(fw))
-im = draw(grid.axes_row[4][1], mesh, fwe, logScale=False, cmap="Blues", **lim(fw))
-im = draw(grid.axes_row[4][2], meshj, fwj, logScale=False, cmap="Blues", **lim(fw))
+draw(grid.axes_row[4][1], mesh, fwe, logScale=False, cmap="Blues", **lim(fw), coverage=cov)
+draw(grid.axes_row[4][2], meshj, fwj, logScale=False, cmap="Blues", **lim(fw), coverage=covj)
 cb = fig.colorbar(im, cax=grid.cbar_axes[4])
 update_ticks(cb, label=labels[4])
 
@@ -115,10 +180,13 @@ phie = fwe + fae + fie
 phij = fwj + faj + fij
 
 im = draw(grid.axes_row[5][0], mesh, phi, logScale=False, cmap="Oranges", cMin=0.3, cMax=0.5)
-im = draw(grid.axes_row[5][1], mesh, phie, logScale=False, cmap="Oranges",  cMin=0.3, cMax=0.5)
-im = draw(grid.axes_row[5][2], meshj, phij, logScale=False, cmap="Oranges",  cMin=0.3, cMax=0.5)
+draw(grid.axes_row[5][1], mesh, phie, logScale=False, cmap="Oranges",  cMin=0.3, cMax=0.5, coverage=cov)
+draw(grid.axes_row[5][2], meshj, phij, logScale=False, cmap="Oranges",  cMin=0.3, cMax=0.5, coverage=covj)
 cb = fig.colorbar(im, cax=grid.cbar_axes[5])
 update_ticks(cb, label=labels[5])
+
+for ax in grid.axes_all:
+    ax.set_facecolor("0.5")
 
 for ax, title in zip(grid.axes_row[0],
                      ["True model", "Conventional inversion + 4PM", "Petrophysical joint inversion"]):
