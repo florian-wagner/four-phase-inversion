507be3cb0fd7c81a30b5c9a47a39fb3a839a47c1
Florian Wagner <mail@fwagner.info>
2018-11-12 19:50:03 -0800

    Renumber regions from top to bottom

--- syn_cases/layered/1_create_model.py
+++ syn_cases/layered/1_create_model.py
@@ -17,24 +17,23 @@ world = mt.createWorld([0, -30], [150, 0], layers=[-5, -15], worldMarker=False)
 block = mt.createPolygon([[60, -5], [90, -5], [100, -15], [50, -15]],
                          isClosed=True)
 geom = mt.mergePLC([world, block])
+geom.addRegionMarker((80, -2.5), 0)
+geom.addRegionMarker((20, -10), 1)
+geom.addRegionMarker((80, -10), 2)
+geom.addRegionMarker((120, -10), 3)
+geom.addRegionMarker((80, -25), 4)
+geom.save("geom.bms")
 
-geom.addRegionMarker((80, -10), 5)
 mesh = mt.createMesh(geom, area=1.0)
 
-for cell in mesh.cells():
-    if cell.marker() == 3:
-        cell.setMarker(4)
-    if cell.marker() == 0:
-        cell.setMarker(3)
-
-for cell in mesh.cells():
-    cell.setMarker(cell.marker() - 1)
+pg.show(mesh, markers=True)
+pg.wait()
 
 # Model creation based on pore fractions
-philayers = np.array([0.4, 0.3, 0.3, 0.2, 0.3])
+philayers = np.array([0.4, 0.3, 0.3, 0.3, 0.2])
 frlayers = 1 - philayers
-fwlayers = np.array([0.3, 0.18, 0.1, 0.02, 0.02])
-filayers = np.array([0.0, 0.1, 0.18, 0.18, 0.28])
+fwlayers = np.array([0.3, 0.18, 0.02, 0.1, 0.02])
+filayers = np.array([0.0, 0.1, 0.28, 0.18, 0.18])
 falayers = philayers - fwlayers - filayers
 
 falayers[np.isclose(falayers, 0.0)] = 0.0
@@ -56,6 +55,7 @@ def to_mesh(data):
 rhotrue = to_mesh(rholayers)
 veltrue = to_mesh(vellayers)
 
+
 # %%
 # Save sensors, true model and mesh
 
@@ -63,6 +63,10 @@ fa = to_mesh(falayers)
 fi = to_mesh(filayers)
 fw = to_mesh(fwlayers)
 fr = to_mesh(frlayers)
+
+fpm.fr = fr
+fpm.phi = 1 - fr
+fpm.show(mesh, rhotrue, veltrue)
 
 assert np.allclose(fa + fi + fw + fr, 1)
 
