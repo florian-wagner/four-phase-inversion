277a8f615b0bf6e771519d48535f803d93fdd1b6
Florian Wagner <mail@fwagner.info>
2019-02-07 14:55:26 +0100

    Presentation plot (BUCHA)

--- syn_cases/layered/plot_presentation.py
+++ syn_cases/layered/plot_presentation.py
@@ -4,9 +4,8 @@ import sys, os
 sys.path.insert(0, os.path.abspath("../.."))
 #############################################
 
-from invlib import add_inner_title, rst_cov, set_style
+from invlib import add_inner_title, rst_cov, set_style, logFormat
 fs = 5.5
-fs = 6
 set_style(fs, style="seaborn-dark")
 
 import matplotlib.pyplot as plt
@@ -57,12 +56,13 @@ def update_ticks(cb, log=False, label="", cMin=None, cMax=None):
     cb.ax.annotate(label, xy=(1, 0.5), xycoords='axes fraction', xytext=(10, 0),
                    textcoords='offset pixels', horizontalalignment='center',
                    verticalalignment='center', rotation=90, fontsize=fs,
-                   fontweight="semibold")
+                   fontweight="regular")
 
 
 def lim(data):
     """Return appropriate colorbar limits."""
     data = np.array(data)
+    print("dMin", data.min(), "dMax", data.max())
     if data.min() < 0:
         dmin = 0.0
     else:
@@ -84,22 +84,23 @@ def draw(ax, mesh, model, **kwargs):
 def minmax(data):
     """Return minimum and maximum of data as a 2-line string."""
     tmp = np.array(data)
+    print("max", tmp.max())
     if np.isclose(tmp.min(), 0, atol=9e-3):
         min = 0
     else:
         min = tmp.min()
     if np.max(tmp) > 10 and np.max(tmp) < 1e4:
-        return "min: %d\nmax: %d" % (min, tmp.max())
+        return "min: %d | max: %d" % (min, tmp.max())
     if np.max(tmp) > 1e4:
-        return "min: %.1e\nmax: %.1e" % (min, tmp.max())
+        return "min: %d" % min + " | max: " + logFormat(tmp.max())
     else:
-        return "min: %.2f\nmax: %.2f" % (min, tmp.max())
+        return "min: %.2f | max: %.2f" % (min, tmp.max())
 
 # %%
-fig = plt.figure(figsize=(7, 4.5))
-grid = ImageGrid(fig, 111, nrows_ncols=(6, 3), axes_pad=[0.15, 0.03],
+fig = plt.figure(figsize=(8.5, 4.5))
+grid = ImageGrid(fig, 111, nrows_ncols=(6, 3), axes_pad=[0.1, 0.03],
                  share_all=True, add_all=True, cbar_location="right",
-                 cbar_mode="edge", cbar_size="5%", cbar_pad=0.08, aspect=True)
+                 cbar_mode="edge", cbar_size="5%", cbar_pad=0.05, aspect=True)
 
 cov = rst_cov(meshj, np.loadtxt("rst_coverage.dat"))
 
@@ -123,10 +124,12 @@ datas = [(veltrue, velest, veljoint),
 
 for i, (row, data, label, cmap) in enumerate(zip(grid.axes_row, datas, labels, cmaps)):
     print("Plotting", label)
+    borderpad = 0.2
     if i == 0:
         lims = {"cMin": 1500, "cMax": 5000}
     elif i == 1:
         lims = {"cMin": 1e3, "cMax": 1e5}
+        borderpad = 0.08
     else:
         lims = lim(list(data[0]) + list(data[1][cov > 0]) + list(data[2][cov > 0]))
     print(lims)
@@ -137,8 +140,10 @@ for i, (row, data, label, cmap) in enumerate(zip(grid.axes_row, datas, labels, c
         color = "k" if j is 0 and i not in (1, 3, 5) else "w"
         ims.append(draw(ax, meshs[j], data[j], **lims,
                    logScale=logScale, coverage=coverage))
-        ax.text(0.987, 0.05, minmax(data[j]), transform=ax.transAxes, fontsize=fs,
-                ha="right", color=color)
+        # ax.text(0.987, 0.05, minmax(data[j]), transform=ax.transAxes, fontsize=fs,
+        #         ha="right", color=color)
+        add_inner_title(ax, minmax(data[j][coverage > 0]), loc=4, size=fs, fw="regular", frame=False,
+                        c=color, borderpad=borderpad)
         ims[j].set_cmap(cmap)
 
     cb = fig.colorbar(ims[0], cax=grid.cbar_axes[i])
@@ -149,54 +154,77 @@ for ax, title, num in zip(grid.axes_row[0], [
         "Petrophysical joint inversion"
 ], "abc"):
     ax.set_title(title, fontsize=fs + 1, fontweight="bold")
-    # ax.set_title("(%s)" % num, loc="left", fontsize=fs + 1, fontweight="bold")
+    ax.set_title("(%s)" % num, loc="left", fontsize=fs + 1, fontweight="bold")
 
 labs = [
-    "inverted", "inverted", "transformed", "transformed", "transformed",
-    "assumed"
+    "Inverted", "Inverted", "Transformed", "Transformed", "Transformed",
+    "Assumed"
 ]
 for ax, lab in zip(grid.axes_column[1], labs):
     add_inner_title(ax, lab, loc=3, size=fs, fw="regular", frame=False,
                     c="w")
 
 labs = [
-    "transformed", "transformed", "inverted", "inverted", "inverted",
-    "assumed & fixed"
+    "Transformed", "Transformed", "Inverted", "Inverted", "Inverted",
+    "Assumed & fixed"
 ]
 
+geom = pg.load("geom.bms")
 if scenario == "Fig2":
     labs[-1] = "inverted"
+
+    # Add labels for covariance reference
+    ax = grid.axes_all[0]
+    mid = geom.xmax()/2
 
 for ax, lab in zip(grid.axes_column[2], labs):
     add_inner_title(ax, lab, loc=3, size=fs, fw="regular", frame=False, c="w")
 
+for i, (ax, label) in enumerate(zip(grid.axes_column[0], long_labels)):
+    ax.set_yticks([-5, -15, -25])
+    ax.set_yticklabels([" 5", "15", "25\n"])
+    ax.set_ylabel("Depth (m)", labelpad=1)
+    color = "k" if i not in (1, 3, 5) else "w"
+    add_inner_title(ax, label, loc=3, c=color, frame=False)
+
+    if i == 5:
+        for bound in geom.boundaries():
+            style = "solid"
+            if np.isclose(bound.size(), 14.14, atol=0.5):
+                style = "dotted"
+            pg.mplviewer.drawSelectedMeshBoundaries(ax, [bound], linewidth=0.5,
+                                                    linestyles=style, color="k")
+    else:
+        pg.mplviewer.drawPLC(ax, geom, fillRegion=False, lw=0.5)
+
 for i, ax in enumerate(grid.axes_all):
     ax.set_facecolor("0.45")
-    ax.plot(sensors, np.zeros_like(sensors), marker="v", lw=0, color="k", ms=1.2)
+    ax.plot(sensors, np.zeros_like(sensors) + 0.1, marker="o", lw=0, color="k",
+            ms=0.6)
     ax.tick_params(axis='both', which='major')
     ax.set_xticks([25, 50, 75, 100, 125])
-    # pg.mplviewer.drawPLC(ax, geom, fillRegion=False, alpha=0.5, lw=0.5)
-    ax.set_aspect(1.5)
+    ax.set_ylim(-25,0)
+    ax.set_aspect(1.85)
 
 for row in grid.axes_row[:-1]:
     for ax in row:
         ax.xaxis.set_visible(False)
 
-for ax in grid.axes_column[-1]:
-    ax.yaxis.set_visible(False)
-
 for ax in grid.axes_row[-1]:
     ax.set_xlabel("x (m)")
 
-for i, (ax, label) in enumerate(zip(grid.axes_column[0], long_labels)):
-    ax.set_yticks([-5, -15, -30])
-    ax.set_yticklabels([" 5", "15", "30\n"])
-    ax.set_ylabel("Depth (m)", labelpad=1)
-    color = "k" if i not in (1, 3, 5) else "w"
-    add_inner_title(ax, label, loc=3, c=color, frame=False)
 
-
-# fig.savefig("4PM_joint_inversion.png", dpi=150, bbox_inches="tight")
-import os
-os.makedirs("agu", exist_ok=True)
-fig.savefig("agu/%s_two_columns.pdf" % scenario, dpi=500, bbox_inches="tight", pad_inches=0.0)
+# fig.savefig("4PM_joint_inversion.pdf", dpi=150, bbox_inches="tight")
+fig.savefig("%s_two_columns_bucha3.pdf" % scenario, dpi=500, bbox_inches="tight", pad_inches=0.0)
+for ax in grid.axes_column[2]:
+    ax.set_visible(False)
+fig.savefig("%s_two_columns_bucha2.pdf" % scenario, dpi=500, bbox_inches="tight", pad_inches=0.0)
+for ax in grid.axes_column[1][2:-1]:
+    ax.set_visible(False)
+fig.savefig("%s_two_columns_bucha1-2.pdf" % scenario, dpi=500, bbox_inches="tight", pad_inches=0.0)
+for ax in grid.axes_column[1][2:]:
+    ax.set_visible(False)
+fig.savefig("%s_two_columns_bucha1-1.pdf" % scenario, dpi=500, bbox_inches="tight", pad_inches=0.0)
+for ax in grid.axes_column[1]:
+    ax.set_visible(False)
+fig.savefig("%s_two_columns_bucha1.pdf" % scenario, dpi=500, bbox_inches="tight", pad_inches=0.0)
